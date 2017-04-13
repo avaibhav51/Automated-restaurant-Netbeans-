1724e64e9dd6de4e802c3e355540f5937acf4fa1
Vaibhav Agarwal <avaibhav51@gmail.com>
2017-04-13 23:31:06 +0530

    Add files via upload

---
+++ restra/src/mini/project/MiniProject.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mini.project;
+
+/**
+ *
+ * @author vaibhav.hp
+ */
+public class MiniProject {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        consumer_table nf1=new consumer_table();
+        nf1.setVisible(true);
+    }
+    
+}
